def7ebe5dcbbb6801d3824220913e47541dffa64
karan kurani <karankurani@hotmail.com>
2010-11-08 19:22:01 -0500

    modified path at unittests.java

--- src/ICS/SND/Tests/UnitTests.java
+++ src/ICS/SND/Tests/UnitTests.java
@@ -1,5 +1,5 @@
 package ICS.SND.Tests;
 
 public class UnitTests {
-    public static final String DATA_PATH = "../ICS-SND/src/ICS/SND/Tests/TestData/";
+    public static final String DATA_PATH = "src/ICS/SND/Tests/TestData/";
 }
